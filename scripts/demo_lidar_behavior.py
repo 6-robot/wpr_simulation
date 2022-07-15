561a02d01c3c0e220035381001591e6e9bf95fce
Robot <zhangwanjie@126.com>
2022-07-15 13:57:52 +0800

    update demo

--- scripts/demo_lidar_behavior.py
+++ scripts/demo_lidar_behavior.py
@@ -11,26 +11,26 @@ count = 0
 def lidar_callback(msg):
     global vel_pub
     global count
-    vel_msg = Twist()
+    vel_cmd = Twist()
     middle = len(msg.ranges)//2
     dist = msg.ranges[middle]
-    rospy.logwarn("正前方测距数值 = %f 米",dist)
+    rospy.loginfo("正前方测距数值 = %f 米",dist)
     if count > 0:
         count = count -1
-        rospy.loginfo("持续转向 count = %d",count)
+        rospy.logwarn("持续转向 count = %d",count)
         return
     if dist < 1.5:
-        vel_msg.angular.z = 0.3
+        vel_cmd.angular.z = 0.3
         count = 50
     else:
-        vel_msg.linear.x = 0.05
-    vel_pub.publish(vel_msg)
+        vel_cmd.linear.x = 0.05
+    vel_pub.publish(vel_cmd)
 
 # 主函数
 if __name__ == "__main__":
     rospy.init_node("lidar_behavior")
-    # 发布机器人运动控制话题
-    vel_pub = rospy.Publisher("cmd_vel",Twist,queue_size=10)
     # 订阅激光雷达的数据话题
     lidar_sub = rospy.Subscriber("scan",LaserScan,lidar_callback,queue_size=10)
+    # 发布机器人运动控制话题
+    vel_pub = rospy.Publisher("cmd_vel",Twist,queue_size=10)
     rospy.spin()
