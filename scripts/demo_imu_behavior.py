4ea8a93af1b0a4839ff20fc1f49d4c4decff8c3b
Robot <zhangwanjie@126.com>
2022-07-20 13:09:03 +0800

    add imu_behavior

--- scripts/demo_imu_behavior.py
+++ scripts/demo_imu_behavior.py
@@ -5,6 +5,7 @@ import rospy
 from sensor_msgs.msg import Imu
 from tf.transformations import euler_from_quaternion
 import math
+from geometry_msgs.msg import Twist
 
 # IMU 回调函数
 def imu_callback(msg):
@@ -23,10 +24,22 @@ def imu_callback(msg):
     pitch = pitch*180/math.pi
     yaw = yaw*180/math.pi
     rospy.loginfo("滚转= %.0f 俯仰= %.0f 朝向= %.0f", roll, pitch, yaw)
+    # 速度消息包
+    vel_cmd = Twist()
+    # 目标朝向角
+    target_yaw = 90
+    # 计算速度
+    diff_angle = target_yaw - yaw
+    vel_cmd.angular.z = diff_angle * 0.01
+    vel_cmd.linear.x = 0.1
+    global vel_pub
+    vel_pub.publish(vel_cmd)
 
 # 主函数
 if __name__ == "__main__":
-    rospy.init_node("imu_data")
-    # 订阅 IMU 数据话题
+    rospy.init_node("imu_behavior")
+    # 订阅 IMU 的数据话题
     imu_sub = rospy.Subscriber("/imu/data",Imu,imu_callback,queue_size=10)
+    # 发布机器人运动控制话题
+    vel_pub = rospy.Publisher("cmd_vel",Twist,queue_size=10)
     rospy.spin()
--- src/demo_imu_behavior.cpp
+++ src/demo_imu_behavior.cpp
@@ -38,6 +38,10 @@
 #include "ros/ros.h"
 #include "sensor_msgs/Imu.h"
 #include "tf/tf.h"
+#include "geometry_msgs/Twist.h"
+
+// 速度消息发布对象（全局变量）
+ros::Publisher vel_pub;
 
 // IMU 回调函数
 void IMUCallback(const sensor_msgs::Imu msg)
@@ -59,16 +63,27 @@ void IMUCallback(const sensor_msgs::Imu msg)
     pitch = pitch*180/M_PI;
     yaw = yaw*180/M_PI;
     ROS_INFO("滚转= %.0f 俯仰= %.0f 朝向= %.0f", roll, pitch, yaw);
+    // 速度消息包
+    geometry_msgs::Twist vel_cmd;
+    // 目标朝向角
+    double target_yaw = 90;
+    // 计算速度
+    double diff_angle = target_yaw - yaw;
+    vel_cmd.angular.z = diff_angle * 0.01;
+    vel_cmd.linear.x = 0.1;
+    vel_pub.publish(vel_cmd);
 }
 
 int main(int argc, char **argv)
 {
     setlocale(LC_ALL, "");
-    ros::init(argc,argv, "demo_imu_data"); 
+    ros::init(argc,argv, "demo_imu_behavior"); 
 
     ros::NodeHandle n;
     // 订阅 IMU 的数据话题
     ros::Subscriber sub = n.subscribe("imu/data", 100, IMUCallback);
+    // 发布速度控制话题
+    vel_pub = n.advertise<geometry_msgs::Twist>("/cmd_vel",10);
     ros::spin();
 
     return 0;
