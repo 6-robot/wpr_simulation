c8038d447bded9da0f0016e9b329bd1ff1473ac3
Robot <zhangwanjie@126.com>
2024-07-06 19:05:43 +0800

    update demo

--- src/demo_cv_hsv.cpp
+++ src/demo_cv_hsv.cpp
@@ -36,7 +36,6 @@
  ********************************************************************/
 
 #include <ros/ros.h>
-#include <image_transport/image_transport.h>
 #include <cv_bridge/cv_bridge.h>
 #include <sensor_msgs/image_encodings.h>
 #include <opencv2/imgproc/imgproc.hpp>
